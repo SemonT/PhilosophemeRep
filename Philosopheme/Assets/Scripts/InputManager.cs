c4e3b24269c917dbe6cf6c1943e2dfec73b6cddb
SemonT <56264177+SemonT@users.noreply.github.com>
2020-03-14 19:34:19 +0400

    Перемещение  и вращение Часть 1

---
+++ Philosopheme/Assets/Scripts/InputManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public GameObject player;
+    private Movement move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = player.GetComponent<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        float mouseX = Input.GetAxisRaw("Mouse X");
+        float mouseY = Input.GetAxisRaw("Mouse Y");
+
+        if (x != 0 || y != 0)  move.MoveOnGround(x, y);
+        if (mouseX != 0 || mouseY != 0) move.Turn(mouseX, mouseY);
+
+
+
+    }
+}
---
+++ Philosopheme/Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 2f;
+    public float strafeCoff = 0.4f;
+
+    // В настройках проекта есть настрока чувствительности мыши, что нужно будет перетащить в настройки игры и эту переменную использовать оттудова
+    public float mouseSense = 1.5f;
+    private Transform cam;
+
+    public void MoveOnGround(float x, float y)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + strafeCoff * transform.right * x + transform.forward * y, speed * Time.deltaTime);
+    }
+
+    public void Turn(float mouseX, float mouseY)
+    {
+        transform.Rotate(0, mouseSense * mouseX, 0, Space.Self);
+
+        if(Mathf.Abs(cam.rotation.x) < 30f)
+            cam.Rotate(mouseSense * -mouseY, 0, 0, Space.Self);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
