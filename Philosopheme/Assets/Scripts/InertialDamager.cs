cc700c7dd611ce185701831d2aea5d82bed911b2
SOUignat <30724436+SOUignat@users.noreply.github.com>
2020-06-21 19:57:26 +0400

    Boxes!

---
+++ Philosopheme/Assets/Scripts/InertialDamager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InertialDamager : MonoBehaviour
+{
+    public float damagePerMps = 20f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Health h = collision.gameObject.GetComponent<Health>();
+        if (h)
+        {
+            h.HealthChange(-collision.relativeVelocity.magnitude * damagePerMps);
+        }
+    }
+}
---
+++ Philosopheme/Assets/Scripts/Shaitan.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaitan : MonoBehaviour
+{
+    public float forceMultiplier = 0.1f;
+    public float minPeriod = 0.1f;
+    public float maxPeriod = 5f;
+    public float minDistance = 3f;
+    public float maxDistance = 15f;
+    float a;
+    float b;
+    float period;
+    float timer;
+
+    Transform playerTransform;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerTransform = Player.instance.transform;
+        a = (maxPeriod - minPeriod) / (maxDistance - minDistance);
+        b = minPeriod - minDistance * a;
+        print("a: " + a + "\t b: " + b);
+        timer = 0;
+        period = 999999999f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > period)
+        {
+            rb.AddForce(RandomVector3(-forceMultiplier / period, forceMultiplier / period), ForceMode.Impulse);
+            timer = 0;
+            print("force (" + period + ")");
+        }
+        float distance = Vector3.Distance(transform.position, playerTransform.position);
+        if (distance >= maxDistance)
+        {
+            period = 999999999f;
+        }
+        else if (distance <= minDistance)
+        {
+            period = minPeriod;
+        }
+        else
+        {
+            period = a * distance + b;
+        }
+        print("distance: " + distance + "\tperiod:" + period);
+    }
+
+    public Vector3 RandomVector3(float min, float max)
+    {
+        return new Vector3(Random.Range(min, max), UnityEngine.Random.Range(min, max), UnityEngine.Random.Range(min, max));
+    }
+}
